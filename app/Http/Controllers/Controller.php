e74e9dea83ab8eae95cab05fc2f6fc919dafe7eb
Queli Hesper <queli.juliani@gmail.com>
2020-09-07 00:01:59 -0300

    primeiro commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function homepage(){
+
+        $variavel = "Homepage sistema de gestão";
+        
+        return view('welcome', [
+            'title' => $variavel
+        ]);
+    }
+
+    public function cadastrar(){
+        echo "Cadastrar";
+    }
+
+    public function fazerLogin(){
+        echo "Fazer Login";
+    }
+}
